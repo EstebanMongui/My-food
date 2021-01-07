d09d6059831bf1e91f0db91057671552f64a121f
Esteban Monguí <esteband.itorres@gmail.com>
2021-01-07 16:00:16 -0500

    feat: Start to create food cards

---
+++ src/components/FoodCards/FoodCards.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+//Styles
+import "./FoodCards.css";
+
+class FoodCards extends React.Component{
+    render(){
+        const{
+            //props
+        }=this.props
+        return(
+            <div className="FoodCards"></div>
+        )
+    }
+}
+
+export default FoodCards;
--- src/pages/MyFoodHome.js
+++ src/pages/MyFoodHome.js
@@ -1,7 +1,9 @@
 import { render } from "@testing-library/react";
 import React from "react";
 
+//Components
 import Header from "../components/Header/Header.js"
+import FoodCards from "../components/FoodCards/FoodCards.js"
 
 class MyFoodHome extends React.Component{
     render(){
@@ -9,9 +11,12 @@ class MyFoodHome extends React.Component{
             //props
         }=this.props
         return(
-            <section>
+            <boddy className="boddy">
                 <Header />
-            </section>
+                <section>
+                    <FoodCards />
+                </section>
+            </boddy>
         )
     }
 }
