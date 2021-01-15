fbe5a3cf15406899c752865f95a974edf487da6c
Esteban Monguí <esteband.itorres@gmail.com>
2021-01-15 10:59:04 -0500

    feat: add state information logic

--- src/components/FoodCards/FoodCards.js
+++ src/components/FoodCards/FoodCards.js
@@ -7,25 +7,25 @@ import Tag from "./components/Tag.js"
 
 class FoodCards extends React.Component{
     render(){
-        const{
-            ProductName="ProductName",
-            Description="Description",
-            Price="$0.0",
-            imageUrl="https://revistapym.com.co/wp-content/uploads/2016/10/las-comidas-rapidas-1.png"
-        }=this.props
         return(
-            <div className="FoodCards">
-                <div className="FoodCards__Description">
-                    <Tag />
-                    <h3>{ProductName}</h3>
-                    <h4>{Description}</h4>
-                </div>
-                <figure className="FoodCards__Product">
-                    <figcaption>
-                        <Tag value={Price}/>
-                    </figcaption>
-                    <img src={imageUrl} alt=""/>
-                </figure>
+            <div className="FoodCards__Container">
+                {this.props.cardInformation.map(card => {
+                    return(
+                        <div className="FoodCards">
+                            <div className="FoodCards__Description">
+                                <Tag />
+                                <h3>{card.ProductName}</h3>
+                                <h4>{card.Description}</h4>
+                            </div>
+                            <figure className="FoodCards__Product">
+                                <figcaption>
+                                    <Tag value={"$" + card.Price}/>
+                                </figcaption>
+                                <img src={card.ImageUrl} alt=""/>
+                            </figure>
+                        </div>
+                    );
+                })}
             </div>
         )
     }
--- src/pages/MyFoodHome.js
+++ src/pages/MyFoodHome.js
@@ -2,10 +2,36 @@ import React from "react";
 //Components
 import Header from "../components/Header/Header.js";
 import Hero from "../components/Hero/Hero.js";
+import FoodCards from "../components/FoodCards/FoodCards.js"
 
 import "./MyFoodHome.css"
 
 class MyFoodHome extends React.Component{
+    state={
+        cardInformation:[
+            {
+                "id":1,
+                "ProductName":"Hamburguer",
+                "Description":"Description",
+                "Price":"0.0",
+                "ImageUrl":"https://revistapym.com.co/wp-content/uploads/2016/10/las-comidas-rapidas-1.png"
+            },
+            {
+                "id":2,
+                "ProductName":"Hamburguer",
+                "Description":"Description",
+                "Price":"0.0",
+                "ImageUrl":"https://revistapym.com.co/wp-content/uploads/2016/10/las-comidas-rapidas-1.png"
+            },
+            {
+                "id":3,
+                "ProductName":"Hamburguer",
+                "Description":"Description",
+                "Price":"0.0",
+                "ImageUrl":"https://revistapym.com.co/wp-content/uploads/2016/10/las-comidas-rapidas-1.png"
+            },
+        ]
+    }
     render(){
         const{
             //props
@@ -15,6 +41,10 @@ class MyFoodHome extends React.Component{
                 <section className="MyFoodHome__Container">
                     <Header />
                     <Hero />
+                    <h1 className="MyFoodHome__SectionTitle"> Delicious Food</h1>
+                    <div className="FoodCards__Container">
+                        <FoodCards cardInformation={this.state.cardInformation}/>
+                    </div>
                 </section>
             </div>
         )
